eb2159d6520f70970ad3c0edc23e213676c52493
TadaoYamaoka <tadaoyamaoka@gmail.com>
2019-03-19 22:47:53 +0900

    訓練データとテストデータを分割

--- dlshogi_zero/csa_to_training_data.py
+++ dlshogi_zero/csa_to_training_data.py
@@ -6,6 +6,7 @@ import glob
 import os.path
 import math
 from collections import defaultdict
+import random
 
 MAX_MOVE_COUNT = 512
 
@@ -13,6 +14,7 @@ MAX_MOVE_COUNT = 512
 def process_csa(database, csa_file_list, filter_moves, filter_rating):
     board = Board()
     parser = Parser()
+    num_positions = 0
     for filepath in csa_file_list:
         parser.parse_csa_file(filepath.encode('utf-8'))
         if parser.endgame not in (b'%TORYO', b'%SENNICHITE', b'%KACHI', b'%HIKIWAKE') or len(parser.moves) < filter_moves:
@@ -54,9 +56,11 @@ def process_csa(database, csa_file_list, filter_moves, filter_rating):
             board.push(move)
 
             chunk.append((hcprs.data, total_move_count, legal_moves.data, visits.data, game_result))
+            num_positions += 1
 
         # write data
         database.write_chunk(chunk)
+    return num_positions
 
 if __name__ == '__main__':
     import argparse
@@ -65,19 +69,43 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('csa_dir', help='directory stored CSA file')
     parser.add_argument('training_database', help='training database file')
+    parser.add_argument('test_database', help='training database file')
     parser.add_argument('--filter_moves', type=int, default=50, help='filter by move count')
     parser.add_argument('--filter_rating', type=int, default=0, help='filter by rating')
+    parser.add_argument('--recursive', '-r', action='store_true')
+    parser.add_argument('--test_ratio', type=float, default=0.1, help='test data ratio')
     parser.add_argument('--clear', action='store_true', help='clear database before processing')
 
     args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
-    csa_file_list = glob.glob(os.path.join(args.csa_dir, '*'))
+    if args.recursive:
+        dir = os.path.join(args.csa_dir, '**')
+    else:
+        dir = args.csa_dir
+    csa_file_list = glob.glob(os.path.join(dir, '*.csa'), recursive=args.recursive)
 
-    training_database = TrainingDataBase(args.training_database, args.clear)
-    training_database.set_model_ver(0)
+    random.shuffle(csa_file_list)
+    traindata_num = int(len(csa_file_list) * (1.0 - args.test_ratio))
+    testdata_num = len(csa_file_list) - traindata_num
+
     logging.info('start process csa')
-    process_csa(training_database, csa_file_list, args.filter_moves, args.filter_rating)
-    training_database.close()
+
+    if traindata_num > 0:
+        logging.info('creating training database')
+        training_database = TrainingDataBase(args.training_database, args.clear)
+        training_database.set_model_ver(0)
+        num_positions = process_csa(training_database, csa_file_list[0:traindata_num], args.filter_moves, args.filter_rating)
+        training_database.close()
+        logging.info('created games:{} positions:{}'.format(traindata_num, num_positions))
+
+    if testdata_num > 0:
+        logging.info('creating test database')
+        test_database = TrainingDataBase(args.test_database, args.clear)
+        test_database.set_model_ver(0)
+        num_positions = process_csa(test_database, csa_file_list[traindata_num:], args.filter_moves, args.filter_rating)
+        test_database.close()
+        logging.info('created games:{} positions:{}'.format(len(csa_file_list) - traindata_num, num_positions))
+
     logging.info('done')
