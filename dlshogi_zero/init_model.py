bbdd8d0192e18eff23d3f3c8123d0602deb07f1a
TadaoYamaoka <tadaoyamaoka@gmail.com>
2019-03-20 00:03:14 +0900

    Add init_model

---
+++ dlshogi_zero/init_model.py
@@ -0,0 +1,14 @@
+﻿from dlshogi_zero.nn.resnet import ResNet
+
+def init_model(model_path):
+    model = ResNet()
+    model.save(model_path)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_path')
+    args = parser.parse_args()
+
+    init_model(args.model_path)
